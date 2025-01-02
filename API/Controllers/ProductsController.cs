717a4756d8ef8ddfc4b6253741d1bf930703b659
Adir Noyman <ofahol2007@hotmail.com>
2025-01-02 22:35:35 +0200

    .

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController(StoreContext context) : ControllerBase
+    {      
+
+        // GET api/products
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetAllProducts()
+        {
+            return await context.Products.ToListAsync();
+        }
+
+        // GET api/products/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProductById(int id)
+        {
+            var product = await context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return product;
+        }
+    }
+}
