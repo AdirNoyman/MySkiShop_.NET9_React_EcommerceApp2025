6db310cc9037d25de23c70a9da9d356c34043ee1
Adir Noyman <ofahol2007@hotmail.com>
2025-02-16 22:34:33 +0200

    .

--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -8,12 +8,42 @@ namespace API.Controllers
     public class BasketController(StoreContext context) : BaseApiController
     {
 
+        // RetrieveBasket method
+        private async Task<Basket?> RetrieveBasket()
+        {
+            return await context.Baskets.Include(basket => basket.BasketItems)
+                .ThenInclude(item => item.Product)
+                .FirstOrDefaultAsync(basket => basket.BasketId == Request.Cookies["basketId"]);
+        }
+
+        // CreateBasket method
+        private Basket CreateBasket()
+        {
+            // Create Id for the new basket
+            var basketId = Guid.NewGuid().ToString();
+            // Create cookie with our customized options
+            var cookieOptions = new CookieOptions
+            {
+                // Even if user refuses to accept cookies, we will still create a cookie
+                IsEssential = true,
+                // Set the expiration date of the cookie for 30 days
+                Expires = DateTime.UtcNow.AddDays(30)
+            };
+
+            Response.Cookies.Append("basketId", basketId, cookieOptions);
+
+            var basket = new Basket {BasketId = basketId};
+
+            context.Baskets.Add(basket);
+
+            return basket;      
+        }
+
+
         [HttpGet]
         public async Task<ActionResult<Basket>> GetBasket()
         {
-            var basket = await context.Baskets.Include(basket => basket.BasketItems)
-                .ThenInclude(item => item.Product)
-                .FirstOrDefaultAsync(basket => basket.BasketId == Request.Cookies["basketId"]);
+            var basket = await RetrieveBasket();
 
             // We will get the basket Id from the user's cookie he sent us via the request and if we can't find the basket we will return 'no content'
             if (basket == null) return NoContent();
@@ -25,18 +55,31 @@ namespace API.Controllers
         [HttpPost]
         public async Task<ActionResult> AddItemToBasket(int productId, int quantity)
         {
-           // Get the client basket
+            // Get the client basket
+            var basket = await RetrieveBasket();
 
-           // If there is no basket, create a new one
+            // If there is no basket (it's null), create a new one
+            basket ??= CreateBasket();
 
-           // Get the product the client wants to add to the basket
+            // Get the product the client wants to add to the basket, by looking it up in the database with the primary key
+            var product = await context.Products.FindAsync(productId);
 
-           // Add the Item (product + quantity) to basket
+            if (product == null) return BadRequest("Problem adding item to basket 😫");
 
-           // Save the changes
+            // Add the Item (product + quantity) to basket
+            basket.AddItem(product, quantity);
 
-           // Return a response back to client
+            // Save the changes to the database
+            var result = await context.SaveChangesAsync() > 0;
+
+            // Return a response back to client
+            if (result) return CreatedAtAction(nameof(GetBasket), basket);
+
+            // If the result is false, return a bad request
+            return BadRequest("Problem updating the basket 😫");
         }
+
+
 
         [HttpDelete]
         public async Task<ActionResult> RemoveItemFromBasket(int productId, int quantity)
@@ -44,7 +87,7 @@ namespace API.Controllers
             // Get the client basket
 
             // If there is no basket, return 'no content'
-            
+
             // Remove the item from the basket or just reduce the quantity
 
             // Save the changes
